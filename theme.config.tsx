fcf363a59000e3495c813f31cd536b806177f6a6
Amy Zhu <szhu0793@uni.sydney.edu.au>
2025-04-20 16:15:10 +1000

    edit1

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,16 +3,5 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Strata Management Portal</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://classic.austlii.edu.au/au/legis/nsw/consol_act/ssma2015242/',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
 }
-
 export default config
